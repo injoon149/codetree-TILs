a643c65571384562e834d5bf63d548dfbf19962a
INJOON CHOI <dlswns8679@naver.com>
2024-04-01 19:06:25 -0500

    Title: 큰 숫자 자리수의 합, Time: 356ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240402/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int printNum(int num)
+{
+    if(1 <= num && num <= 9) return num;
+    else return printNum(num / 10) + printNum(num % 10); 
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << printNum(a*b*c);
+    return 0;
+}
