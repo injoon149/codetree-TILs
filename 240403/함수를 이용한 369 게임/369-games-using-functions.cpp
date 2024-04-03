4adba1b29e716f4b48cf4eb028b60600d990b644
INJOON CHOI <dlswns8679@naver.com>
2024-04-02 19:30:53 -0500

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool IsMagicNumber(int num)
+{
+    if(num % 3 == 0) return 1;
+    else {
+        for(int i = 0; i<= 6; i++)
+        {
+            if(num == 0) return 0;
+            else {
+                if((num %10)%3 == 0) {
+                    return 1;
+                }
+                else num = num / 10;
+            }
+        }
+    }
+    return 0;
+}
+
+int solution(int a, int b)
+{
+    int num = 0;
+    for(int i = a; i<=b; i++)
+    {
+        if(IsMagicNumber(i) == 1)
+        {
+            num++;
+        }
+
+    }
+    return num;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    cout << solution(a, b);
+    return 0;
+}
