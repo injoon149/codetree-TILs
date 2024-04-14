9d8461312d170efc5c8c28885555ba3f46166f70
INJOON CHOI <dlswns8679@naver.com>
2024-04-14 14:24:43 -0500

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240415/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+bool check1(int m)
+{
+    if(1<= m && m<= 12) return 1;
+    else return 0;
+}
+
+bool solution(int m, int d)
+{
+    if(check1(m) == 1) {
+        if(m == 2) {
+            if(1<=d && d<= 28){
+                return 1;
+            }
+        }
+        else if(m %2 == 0)
+        {
+            if(1<=d && d<=30){
+                return 1;
+            }
+        }
+        else if(m %2 == 1)
+        {
+            if(1<=d && d<= 31){
+                return 1;
+            }
+        }
+
+    }
+    return 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, d;
+    cin >> m >> d;
+    if(solution(m,d) == 1) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
