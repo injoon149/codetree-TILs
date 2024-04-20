58e00c4d1be6f3dbcfe69f3db94b14c391cd6a42
INJOON CHOI <dlswns8679@naver.com>
2024-04-20 20:23:50 +0900

    Title: palindrome 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240420/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string modify(string &s)
+{
+    string a = s;
+    int k = a.length() /2;
+    for(int i = 0; i<k; i++)
+    {
+        if()
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
