7c79248c417587e74ee730340c870a5d04001bf7
INJOON CHOI <dlswns8679@naver.com>
2024-04-20 20:25:20 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 240420/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -2,17 +2,22 @@
 #include <string>
 using namespace std;
 
-string modify(string &s)
-{
-    string a = s;
-    int k = a.length() /2;
-    for(int i = 0; i<k; i++)
+string str;
+
+bool Palindrome(string &s) {
+    for(int i = 0; i<(int)s.size(); i++)
     {
-        if()
+        if(s[i] != s[(int)s.size() - i -1])
+            return false;
     }
+    return true;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    cin >> str;
+
+    if(Palindrome(str)) cout << "Yes";
+    else cout << "No";
     return 0;
 }
