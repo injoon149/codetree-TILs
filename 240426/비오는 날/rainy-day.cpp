bfc19574bee84c00d1a5a49d6db9be2d1ddc8358
INJOON CHOI <dlswns8679@naver.com>
2024-04-26 20:12:09 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240426/비오는 날/rainy-day.cpp
+++ 240426/비오는 날/rainy-day.cpp
@@ -1,64 +1,38 @@
 #include <iostream>
 #include <string>
-#include <tuple>
+
 using namespace std;
 
-tuple<string, string, string> data[100];
+class Forecast {
+    public:
+        string date, day, weather;
+        Forecast(string date, string day, string weather){
+            this->date = date;
+            this->day = day;
+            this->weather = weather;
+        }
+};
 
+Forecast ans = Forecast("9999-99-99", "", "");
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    tuple<int, int, int> first = make_tuple(2100, 12, 31);
+int main()
+{
     int n;
     cin >> n;
-    string date, day, weather;
-    int y, m, d;
-    string daydream;
-    for(int i = 0; i<n; i++)
+    for(int i = 1; i<=n; i++)
     {
+        string date, day, weather;
         cin >> date >> day >> weather;
-        data[i] = make_tuple(date, day, weather);
-        y = stoi(date.substr(0, 4));
-        m = stoi(date.substr(5, 2));
-        d = stoi(date.substr(8, 2));
 
+        Forecast f = Forecast(date, day, weather);
         if(weather == "Rain")
         {
-            if(y < get<0>(first))
+            if(ans.date >= f.date)
             {
-                first = make_tuple(y,m,d);
-                daydream = day;
-            }
-            else if(y == get<0>(first))
-            {
-                if(m < get<1>(first))
-                {
-                    first = make_tuple(y,m,d);
-                    daydream = day;
-                }
-                else if(m == get<1>(first))
-                {
-                    if(d < get<2>(first))
-                    {
-                        first = make_tuple(y,m,d);
-                        daydream = day;
-                    }
-                }
+                ans = f;
             }
         }
     }
-    if(get<1>(first) < 10 && get<2>(first) >=10)
-    {
-        cout << get<0>(first) <<"-0"<<get<1>(first)<<"-"<<get<2>(first)<<" "<<daydream<<" Rain"; 
-    }
-    else if(get<1>(first) <10 && get<2>(first) < 10)
-    {
-        cout << get<0>(first) <<"-0"<<get<1>(first)<<"-0"<<get<2>(first)<<" "<<daydream<<" Rain"; 
-    }
-    else if(get<1>(first)>=10 && get<2>(first) < 10)
-    {
-        cout << get<0>(first) <<"-"<<get<1>(first)<<"-0"<<get<2>(first)<<" "<<daydream<<" Rain"; 
-    }
-    else cout << get<0>(first) <<"-"<<get<1>(first)<<"-"<<get<2>(first)<<" "<<daydream<<" Rain"; 
+    cout << ans.date <<" " << ans.day << " " << ans.weather;
     return 0;
 }
