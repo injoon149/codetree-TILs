f94c7a38b45a3e5495d93935e8413f885e692ab8
INJOON CHOI <dlswns8679@naver.com>
2024-05-03 16:18:04 +0900

    Title: 여러가지 진수변환, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240503/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string change(int a, int b)
+{
+    string s;
+    while(true)
+    {
+        if(a == 0) break;
+        s += to_string(a%b);
+        a = a/4;
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    cout << change(a,b);
+
+    return 0;
+}
