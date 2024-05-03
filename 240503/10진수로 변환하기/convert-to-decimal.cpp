5b2b975bbedd1c337b1020b48f385126db39ffaf
INJOON CHOI <dlswns8679@naver.com>
2024-05-03 14:40:41 +0900

    Title: 10진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240503/10진수로 변환하기/convert-to-decimal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string binary;
+    cin >> binary;
+
+    int num = 0;
+    for(int i = 0; i<(int)binary.size(); i++)
+    {
+        num = num*2 + (binary[i]- '0');
+    }
+    cout << num;
+    return 0;
+}
