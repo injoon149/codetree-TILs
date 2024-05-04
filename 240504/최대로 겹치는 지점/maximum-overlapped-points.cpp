a369e2cd2d2fd4f3e80421d1f4e7dd2cd11d9b65
INJOON CHOI <dlswns8679@naver.com>
2024-05-04 19:58:39 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240504/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, x1, x2;
+    cin >> n;
+    vector<int> v(101, 0);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> x1 >> x2;
+        for(int j = x1; j<= x2; j++)
+        {
+            v[j] += 1;
+        }
+    }
+    cout << *max_element(v.begin(), v.end());
+    return 0;
+}
