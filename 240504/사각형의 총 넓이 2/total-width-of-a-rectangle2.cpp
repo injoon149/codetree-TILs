5b2779b3a4b3b0d282ab200ee3719cde557287b0
INJOON CHOI <dlswns8679@naver.com>
2024-05-04 20:09:54 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240504/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240504/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -6,7 +6,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n, x1, y1, x2, y2;
     cin >> n;
-    vector<vector<int>> v(101, vector<int>(101, 0));
+    vector<vector<int>> v(201, vector<int>(201, 0));
     while(n--)
     {
         cin >> x1 >> y1 >> x2 >> y2;
@@ -14,14 +14,14 @@ int main() {
         {
             for(int j = y1; j<y2; j++)
             {
-                v[i][j] = 1;
+                v[i+100][j+100] = 1;
             }
         }
     }
     int cnt = 0;
-    for(int i = 0; i<101; i++)
+    for(int i = 0; i<201; i++)
     {
-        for(int j = 0; j<101; j++)
+        for(int j = 0; j<201; j++)
         {
             if(v[i][j] == 1) cnt++;
         }
