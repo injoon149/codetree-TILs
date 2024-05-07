ca40dd545173b66674fa4b93b1c79afbc0c5843c
INJOON CHOI <dlswns8679@naver.com>
2024-05-08 05:11:10 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240508/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+bool InRange(int x, int y, int n, int m)
+{
+    return (0<=x && x<n && 0<=y && y<m);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n, vector<int>(m, 0));
+    int nx = 1;
+    v[0][0] = 1;
+    int dir_num = 0;
+    int x = 0, y = 0;
+    for(int i = 2; i<= n*m; i++)
+    {
+        
+        int nx = x + dx[dir_num];
+        int ny = y + dy[dir_num];
+        if(InRange(nx, ny, n, m) && v[nx][ny] == 0)
+        {
+            x = nx;
+            y = ny;
+            v[x][y] = i;
+        }
+        else
+        {
+            dir_num = (dir_num + 1) % 4;
+            i--;
+        }
+    }
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<m; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
