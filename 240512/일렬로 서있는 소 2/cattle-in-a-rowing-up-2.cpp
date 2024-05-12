3fb236ad2ae8707f7cc83a2ad00b459a0bf9e4bb
INJOON CHOI <dlswns8679@naver.com>
2024-05-12 20:36:32 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define MAX_N 100
+using namespace std;
+
+int n;
+int arr[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int cnt = 0;
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = i+1; j<n; j++)
+        {
+            for(int k = j+1; k<n; k++)
+            {
+                if(arr[i] <= arr[j] && arr[j] <= arr[k])
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
