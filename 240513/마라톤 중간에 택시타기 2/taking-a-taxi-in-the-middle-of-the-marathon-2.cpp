d7eaa0836da3880e920d82ba01c97ce127b6079b
INJOON CHOI <dlswns8679@naver.com>
2024-05-13 18:14:13 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <cstdlib>
+
+#define MAX_N 100
+using namespace std;
+
+int n;
+int x[MAX_N], y[MAX_N];
+
+int main() {
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> x[i] >> y[i];
+    }
+    int ans = INT_MAX;
+    for(int i = 1; i<n-1; i++)
+    {
+        int dist = 0;
+        int prev_idx = 0;
+        for(int j = 1; j<n; j++)
+        {
+            if(j == i) continue;
+            dist += abs(x[prev_idx] - x[j]) + abs(y[prev_idx] - y[j]);
+            prev_idx = j;
+        }
+        ans = min(ans, dist);
+    }
+    cout << ans;
+    return 0;
+}
