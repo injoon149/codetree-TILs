bda6899b3b7ac7d62f6babb4730922c03626deee
INJOON CHOI <dlswns8679@naver.com>
2024-05-15 14:17:15 +0900

    Title: 최고의 13위치 2, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240515/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#define MAX_N 20
+using namespace std;
+
+int n;
+int arr[MAX_N][MAX_N];
+
+int main()
+{
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int max_cnt = 0;
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n-2; j++)
+        {
+            for(int k = 0; k<n; k++)
+            {
+                for(int l = 0; l<n-2; l++)
+                {
+                    if(i == k && abs(j-l) <= 2) continue;
+
+                    int cnt1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                    int cnt2 = arr[k][l] + arr[k][l+1] + arr[k][l+2];
+                    max_cnt = max(max_cnt, cnt1 + cnt2);
+                }
+            }
+        }
+    }
+    cout << max_cnt;
+    return 0;
+}
