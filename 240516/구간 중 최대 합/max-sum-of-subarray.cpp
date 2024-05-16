d2a44a25d68c0120c41dea30206cfd7b679aae47
INJOON CHOI <dlswns8679@naver.com>
2024-05-16 10:38:12 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n, 0);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    int max = 0;
+    int cnt = 0;
+    for(int i = 0; i<n-k; i++)
+    {
+        for(int j = i; j<i+k; j++)
+        {
+            cnt += v[j];
+        }
+        if(max < cnt) max = cnt;
+        cnt = 0;
+    }
+    cout << max;
+    return 0;
+}
