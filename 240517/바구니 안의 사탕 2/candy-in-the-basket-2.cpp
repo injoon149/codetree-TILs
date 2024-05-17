4dfcdca1b7b3ff8fd7f122f57c2dd5270033026a
INJOON CHOI <dlswns8679@naver.com>
2024-05-17 17:21:57 +0900

    Title: 바구니 안의 사탕 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240517/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(201, 0);
+    int candy, basket;
+    int max = 0;
+    int cnt = 0;
+
+    for(int i = 0; i<n; i++)
+    {
+        cin >> candy >> basket;
+        v[basket] += candy;
+    }
+    for(int i = 0; i<101; i++)
+    {
+        cnt = 0;
+        if(i >= k)
+        {
+            for(int j = i-k; j<=i+k; j++)
+            {
+                cnt += v[j];
+            }
+            if(max < cnt) max = cnt;
+        }
+    }
+    cout << max;
+    return 0;
+    
+}
