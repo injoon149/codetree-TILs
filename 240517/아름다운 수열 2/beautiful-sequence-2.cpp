91127559bf747c49429084dae23ce4ea1d785372
INJOON CHOI <dlswns8679@naver.com>
2024-05-17 17:08:20 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240517/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 100
+using namespace std;
+
+int n, m;
+int arr1[MAX_N], arr2[MAX_N];
+int tmp[MAX_N];
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i<n; i++) cin >> arr1[i];
+    for(int i = 0; i<m; i++) cin >> arr2[i];
+
+    sort(arr2, arr2+m);
+
+    int cnt = 0;
+    for(int i = 0; i<= n-m; i++) {
+        for(int j = 0; j<m; j++)
+        {
+            tmp[j] = arr1[i+j];
+        }
+        sort(tmp, tmp+m);
+
+        bool issame = true;
+        for(int j = 0; j<m; j++)
+        {
+            if(tmp[j] != arr2[j]) {
+                issame = false;
+                break;
+            }
+        }
+        if(issame) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
