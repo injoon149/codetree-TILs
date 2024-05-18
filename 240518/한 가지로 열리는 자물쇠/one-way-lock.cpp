ba078248f4e08a4ff05b2f758097ca1905313a6b
INJOON CHOI <dlswns8679@naver.com>
2024-05-18 20:49:57 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240518/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int a, b, c;
+    cin >> a >> b >> c;
+    int cnt = 0;
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 1;  j<=n; j++)
+        {
+            for(int k = 1; k<=n; k++)
+            {
+                if(abs(i-a)<=2 || abs(j-b)<=2 || abs(k-c)<= 2)
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
