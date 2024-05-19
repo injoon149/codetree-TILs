872616a37a3091d5685a02f41b8da97d0c4103a3
INJOON CHOI <dlswns8679@naver.com>
2024-05-19 21:19:46 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240519/개발자의 능력 2/ability-of-developer-2.cpp
+++ 240519/개발자의 능력 2/ability-of-developer-2.cpp
@@ -15,8 +15,8 @@ int main()
     int first = set[0] + set[5];
     int second = set[1] + set[4];
     int third = set[2] + set[3];
-    int min_num = min(first, second);
-    int max_num = max(second, third);
+    int min_num = min(first, min(second, third));
+    int max_num = max(second, max(second, third));
     cout << abs(max_num - min_num);
     return 0;
 }
