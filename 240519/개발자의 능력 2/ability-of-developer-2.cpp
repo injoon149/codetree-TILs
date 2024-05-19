07376f369790d3e521b0129b3c8e1478bf8195ac
INJOON CHOI <dlswns8679@naver.com>
2024-05-19 21:18:56 +0900

    Title: 개발자의 능력 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240519/개발자의 능력 2/ability-of-developer-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int set[6];
+    int num;
+    for(int i = 0; i<6; i++)
+    {
+        cin >> set[i];
+    }
+    sort(set, set+6);
+
+    int first = set[0] + set[5];
+    int second = set[1] + set[4];
+    int third = set[2] + set[3];
+    int min_num = min(first, second);
+    int max_num = max(second, third);
+    cout << max_num - min_num;
+    return 0;
+}
