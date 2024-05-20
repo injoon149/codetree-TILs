1ddfd5d99d982ce3a8ddf6f46f3cc033bed029c5
INJOON CHOI <dlswns8679@naver.com>
2024-05-20 12:34:04 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240520/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> x1[i] >> x2[i];
+    }
+    int ans = 0;
+    for(int i = 0; i<n; i++)
+    {
+        bool overlap = false;
+        for(int j = 0; j<n; j++)
+        {
+            if(j == i) continue;
+            if((x1[i] <= x1[j] && x2[i] >= x2[j]) || (x1[i] >= x1[j] && x2[i] <= x2[j]))
+            {
+                overlap = true;
+                break;
+            }
+        }
+        if(overlap == false) ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
