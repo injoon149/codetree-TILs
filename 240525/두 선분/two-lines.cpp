601d693eea729f22188da4e2d3c9e5cc0e3e212e
INJOON CHOI <dlswns8679@naver.com>
2024-05-25 20:48:23 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240525/두 선분/two-lines.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x1, x2, x3, x4;
+    cin >> x1 >> x2 >> x3 >> x4;
+    if(x2 < x3 || x4 < x1) cout << "nonintersecting";
+    else cout << "intersecting";
+    return 0;
+}
