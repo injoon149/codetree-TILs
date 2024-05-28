a9cd49e80b5d60bcc72046af3a649aa22039a74d
INJOON CHOI <dlswns8679@naver.com>
2024-05-29 00:45:26 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240529/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 3
+using namespace std;
+
+int n = 3;
+int a[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i = 0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+
+    if(a[0] + 1 == a[1] && a[1] +1 == a[2])
+    {
+        cout << 0;
+    }
+    else if(a[0] + 2 == a[1] || a[1] + 2 == a[2])
+    {
+        cout << 1;
+    }
+    else cout << 2;
+    return 0;
+}
