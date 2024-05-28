5d612079d06d25c45e6fd9b1542270ee05cd8094
INJOON CHOI <dlswns8679@naver.com>
2024-05-29 00:36:52 +0900

    Title: 순 간 이 동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240529/순 간 이 동/teleportation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,x,y;
+    cin >> a >> b >> x >> y;
+    int start = a;
+    int first_num = 0;
+    first_num += abs(a - x);
+    first_num += abs(b - y);
+
+    int second_num = 0;
+    second_num += abs(a - y);
+    second_num += abs(b - x);
+
+    int min_num = min(first_num, second_num);
+    cout << min_num;
+    return 0;
+}
