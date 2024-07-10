9244f6c044fc75415253b2cce1656488c5f1aa82
INJOON CHOI <dlswns8679@naver.com>
2024-07-10 15:01:51 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    int num;
+    vector<int> v;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> num;
+        v.push_back(num);
+    }
+    int max = 0;
+    vector<int> v2(n, 0);
+    for(int i = 0; i<n-1; i++)
+    {
+        for(int j = i+1; j<n; j++)
+        {
+            int bomb_name = 0;
+            int bomb_explode = 0;
+            if(v[i] == v[j])
+            {
+                if(j-i <= k)
+                {
+                    v2[v[i]]++;
+                }
+            }
+        }
+    }
+    int result = 0;
+    for(int i = 0; i<v2.size(); i++)
+    {
+        if(max < v2[i])
+        {
+            max = v2[i];
+            result = i;
+        }
+    }
+    if(max == 0) cout << max;
+    else cout << result;
+    return 0;
+}
