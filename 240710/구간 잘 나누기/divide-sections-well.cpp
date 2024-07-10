ce3e70cee4a4b3d53f36be267ed83028b59793de
INJOON CHOI <dlswns8679@naver.com>
2024-07-10 15:54:48 +0900

    Title: 구간 잘 나누기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_A 10000
+#define MAX_N 100
+using namespace std;
+
+int n, m;
+int a[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 0; i<n; i++) 
+    {
+        cin >> a[i];
+    }
+    int ans = MAX_A;
+    for(int i = 1; i<=MAX_A; i++)
+    {
+        bool possible = true;
+        int section = 1;
+
+        int cnt = 0;
+        for(int j = 0; j<n; j++)
+        {
+            if(a[j] > i) {
+                possible = false;
+                break;
+            }
+            if(cnt + a[j] > i) {
+                cnt = 0;
+                section++;
+            }
+            cnt += a[j];
+        }
+        if(possible && section <= m) {
+            ans = min(ans, i);
+        }
+    }
+    cout << ans;
+    return 0;
+}
