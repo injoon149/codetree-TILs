1bbe15d6f4fdc2b2bdacea502dc5c85e266c02f9
INJOON CHOI <dlswns8679@naver.com>
2024-08-08 13:09:44 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
+++ 240808/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -1,86 +1,62 @@
 #include <iostream>
 #include <vector>
+
+#define DIR_NUM 4
+#define MAX_N 100
 using namespace std;
 
 int n;
+int curr_x, curr_y;
+int a[MAX_N+1][MAX_N+1];
 
-bool InRange(int x, int y)
-{
-    return 1 <= x && x <= n && 1 <= y && y <=n;
+vector<int> visited_nums;
+
+bool InRange(int x, int y) {
+    return 1 <= x && x <= n && 1 <= y && y <= n;
+}
+
+bool CanGo(int x, int y, int curr_num) {
+    return InRange(x, y) && a[x][y] > curr_num;
+}
+
+bool Simulate() {
+    int dx[DIR_NUM] = {-1,1,0,0};
+    int dy[DIR_NUM] = {0,0,-1,1};
+
+    for(int i = 0; i<DIR_NUM; i++) {
+        int next_x = curr_x + dx[i];
+        int next_y = curr_y + dy[i];
+
+        if(CanGo(next_x, next_y, a[curr_x][curr_y])) {
+            curr_x = next_x;
+            curr_y = next_y;
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int r, c;
-    cin >> n >> r >> c;
-    vector<vector<int>> v(n, vector<int>(n, 0));
-    for(int i = 0; i<n; i++)
-    {
-        for(int j = 0; j<n; j++)
+    cin >> n >> curr_x >> curr_y;
+
+    for(int i = 1; i<=n; i++) {
+        for(int j = 1; j<=n; j++)
         {
-            cin >> v[i][j];
+            cin >> a[i][j];
         }
     }
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0,0,-1,1};
+    visited_nums.push_back(a[curr_x][curr_y]);
 
-    int max_num = 0;
-    r = r-1;
-    c = c-1;
-    int max = 0;
-    int startx = 0, starty = 0;
-    vector<int> v2;
-    v2.push_back(v[r][c]);
-    vector<int> flag(5, 0);
+    while(true) {
+        bool greater_number_exist = Simulate();
 
-    
-
-
-
-    while(1)
-    {
-        for(int i = 1; i<=4; i++)
-        {
-            flag[i] = 1;
-        }
-
-        for(int i = 0; i<4; i++)
-        {
-            if(InRange(r + dx[i], c + dy[i]))
-            {
-                max = v[r][c];
-                if(v[r+dx[i]][c+dy[i]] > max)
-                {
-                    startx = r + dx[i];
-                    starty = c + dy[i];
-                    v2.push_back(v[startx][starty]);
-                    r = startx;
-                    c = starty;
-                }
-                else
-                {
-                    flag[i+1] = 0;
-                }
-            }
-            else flag[i+1] = 0;
-        }
-        if(flag[1] == 0 && flag[2] == 0 && flag[3] == 0 && flag[4] == 0)
-        {
-            for(int i = 0; i<v2.size(); i++)
-            {
-                cout << v2[i] <<" ";
-            }
-            return 0;
-        }
+        if(!greater_number_exist) break;
+        visited_nums.push_back(a[curr_x][curr_y]);
     }
 
-    for(int i = 0; i<v2.size(); i++)
+    for(int i = 0; i<(int)visited_nums.size(); i++)
     {
-        cout << v2[i] <<" ";
+        cout << visited_nums[i] << " ";
     }
-
-
-
-
     return 0;
 }
