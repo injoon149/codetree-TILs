3e27191c541e290ffa9135c5eb12d2fa1710caa5
INJOON CHOI <dlswns8679@naver.com>
2024-08-21 16:31:08 +0900

    Title: 정수 사각형 최소 합, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
+++ 240821/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
@@ -23,13 +23,13 @@ int main() {
     } 
     for(int i = 1; i<n; i++)
     {
-        dp[i][0] = dp[i-1][0] + v[i][0]; 
+        dp[i][n-1] = dp[i-1][n-1] + v[i][n-1]; 
     }
-    for(int i = n-2; i>=0; i--)
+    for(int i = 1; i<n; i++)
     {
-        for(int j = 0; j<n; j++)
+        for(int j = n-2; j>=0; j--)
         {
-            dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + v[i][j];
+            dp[i][j] = min(dp[i][j+1], dp[i-1][j]) + v[i][j];
         }
     }
     cout << dp[n-1][0];
