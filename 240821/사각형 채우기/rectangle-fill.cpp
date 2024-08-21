0dfdff99a3f087bb0f3932ceee60b03341948f70
INJOON CHOI <dlswns8679@naver.com>
2024-08-21 09:35:06 +0900

    Title: 사각형 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i<=1000; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    int n;
+    cin >> n;
+    cout << dp[n];
+    return 0;
+}
