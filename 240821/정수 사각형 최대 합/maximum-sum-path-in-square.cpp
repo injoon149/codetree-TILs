7ad45a965d6a5c803d9bd0e87bd37681dc3dd2bd
INJOON CHOI <dlswns8679@naver.com>
2024-08-21 14:23:07 +0900

    Title: 정수 사각형 최대 합, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240821/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240821/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -25,11 +25,11 @@ int main() {
     {
         dp[i][i] = dp[i-1][i-1] + v[i][i];
     }
-    for(int i = 1; i<n; i++)
+    for(int i = 2; i<n; i++)
     {
         for(int j = 1; j<=i; j++)
         {
-            if(i == 1 && j == 1) continue;
+            
             dp[i][j] = max(dp[i-1][j] + v[i][j], dp[i][j-1] + v[i][j]);
         }
     }
