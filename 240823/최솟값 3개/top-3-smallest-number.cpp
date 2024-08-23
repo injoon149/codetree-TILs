9c6e2890610c8380ac2027aca45d936789fa7abc
INJOON CHOI <dlswns8679@naver.com>
2024-08-23 10:17:59 +0900

    Title: 최솟값 3개, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240823/최솟값 3개/top-3-smallest-number.cpp
+++ 240823/최솟값 3개/top-3-smallest-number.cpp
@@ -5,31 +5,21 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n, num;
     cin >> n;
-    int x1,x2,x3;
-    long long result, result2;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for(int i = 0; i<n; i++)
-    {
+    vector<int> smallest;
+    for(int i = 0; i < n; i++) {
         cin >> num;
-        pq.push(num);
-        if(pq.size() < 3) cout <<"-1" <<endl;
-        else
-        {
-            x1 = pq.top();
-            pq.pop();
-            x2 = pq.top();
-            pq.pop();
-            x3 = pq.top();
-            pq.pop();
-            result = x1 * x2;
-            result2 = result * x3;
-            cout << result2 << endl;
-            pq.push(x1);
-            pq.push(x2);
-            pq.push(x3);
+        smallest.push_back(num);
+        if(smallest.size() < 3) {
+            cout << "-1" << endl;
+        } else {
+            if(smallest.size() > 3) {
+                smallest.pop_back();
+            }
+            sort(smallest.begin(), smallest.end());
+            long long result = (long long)smallest[0] * smallest[1] * smallest[2];
+            cout << result << endl;
         }
     }
     return 0;
