93f3932c4a9427e072b31a535fa35e48e6479b5d
INJOON CHOI <dlswns8679@naver.com>
2024-08-26 10:18:40 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 28ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240826/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    vector<int> v(n, 0);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    int currentSum = v[0];
+    int maxSum = v[0];
+
+    //합이 0보다 작아지면 현재 합을 v[i]로 바꿔버린다
+    for(int i = 1; i<n; i++)
+    {
+        if(currentSum + v[i] < 0)
+        {
+            currentSum = v[i];
+        }
+        else currentSum = currentSum + v[i];
+        maxSum = max(maxSum, currentSum);
+    }
+    cout << maxSum;
+
+
+    return 0;
+}
