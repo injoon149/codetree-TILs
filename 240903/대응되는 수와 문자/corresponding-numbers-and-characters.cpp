e217a76d8bd5828beeae835ed14594dbb48ded0b
INJOON CHOI <dlswns8679@naver.com>
2024-09-03 14:42:37 +0900

    Title: 대응되는 수와 문자, Time: 1095ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

---
+++ 240903/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    unordered_map<string, int> map_num;
+    string s;
+    string st;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> s;
+        map_num[s] = i+1;
+    }
+    for(int i = 0; i<m; i++)
+    {
+        cin >> st;
+        for(auto iter = map_num.begin(); iter != map_num.end(); iter++)
+        {
+            if(iter->first == st)
+            {
+                cout << iter->second << endl;
+            }
+            else if(isdigit(st[0]))
+            {
+                if(iter->second == stoi(st))
+                {
+                    cout << iter->first << endl;
+                }
+                
+            }
+        }
+    }
+
+    return 0;
+}
