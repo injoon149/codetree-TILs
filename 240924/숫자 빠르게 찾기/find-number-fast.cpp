207779a9f820d128905766d80cf0cf4dac7e1dda
INJOON CHOI <dlswns8679@naver.com>
2024-09-24 08:13:32 +0900

    Title: 숫자 빠르게 찾기, Time: 1096ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240924/숫자 빠르게 찾기/find-number-fast.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binary_search(long long num, vector<long long> v)
+{
+    int left = 0;
+    int right = v.size()-1;
+
+    while(left <= right)
+    {
+        int mid = (left + right) / 2;
+        if(v[mid] == num) return mid+1;
+        if(v[mid] > num) right = mid -1;
+        else left = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin >> n >> m;
+    vector<long long> v(n, 0);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    long long num;
+    for(int i = 0; i<m; i++)
+    {
+        cin >> num;
+        cout << binary_search(num, v) << endl;
+    }
+    return 0;
+}
