2bc9086648e58dc8c5c956353bb9674c8ec4ca6f
INJOON CHOI <dlswns8679@naver.com>
2024-09-24 11:39:56 +0900

    Title: 가장 먼저 나오는 숫자, Time: 213ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240924/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int lowerbound(long long target, const vector<long long>& v)
+{
+    int left = 0;
+    int right = v.size() - 1;
+    int min_idx = v.size();
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(v[mid] >= target) {
+            right = mid - 1;
+            min_idx = min(min_idx, mid);
+        }
+        else left = mid + 1;
+    }
+    return min_idx;
+}
+
+int upperbound(long long target, const vector<long long>& v)
+{
+    int left = 0;
+    int right = v.size() - 1;
+    int min_idx = v.size();
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(v[mid] > target) {
+            right = mid - 1;
+            min_idx = min(min_idx, mid);
+        }
+        else left = mid + 1;
+    }
+    return min_idx;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<long long> v(n, 0);
+    for(int i = 0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    long long num;
+    for(int i = 0; i<m; i++)
+    {
+        cin >> num;
+        if(upperbound(num, v) - lowerbound(num, v) == 0)
+        {
+            cout << -1 << endl;
+        }
+        else cout << lowerbound(num, v) + 1 << endl;
+    }
+    return 0;
+}
