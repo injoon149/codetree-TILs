aa89c60f5bfdda701629ad392d13b9fe5c2bc2c4
INJOON CHOI <dlswns8679@naver.com>
2024-09-25 15:26:12 +0900

    Title: 회의실 겹치지 않게 하기, Time: 47ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240925/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+bool compare(pair<int,int> a, pair<int,int> b)
+{
+    return a.second < b.second;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,s,e;
+    cin >> n;
+    vector<pair<int,int>> v;
+    int cnt = 0;
+    for(int i = 0; i<n; i++)
+    {
+        cin >> s >> e;
+        v.push_back(make_pair(s,e));
+    }
+    sort(v.begin(), v.end(), compare);
+    int last_e = -1;
+    for(int i = 0; i<v.size(); i++)
+    {
+        int s,e;
+        tie(s,e) = v[i];
+        if(last_e < s)
+        {
+            cnt++;
+            last_e = s;
+        }
+    }
+    
+    cout << n- cnt;
+
+    return 0;
+}
